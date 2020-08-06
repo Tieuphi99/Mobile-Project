43518a0936143aefe2405b2409bf4ca97d49c21a
Tieuphi <tieuphi99@gmail.com>
2020-08-06 17:32:37 +0700

    add sword hitbox

---
+++ Assets/Scripts/PlayerScripts/Attack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+    }
+}
