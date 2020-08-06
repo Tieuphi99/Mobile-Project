1d7cc7482496f18a726120fe8267e1becd178df5
Tieuphi <tieuphi99@gmail.com>
2020-08-06 16:32:56 +0700

    add spider idle, walk

--- Assets/Scripts/EnemyScripts/MossGiant.cs
+++ Assets/Scripts/EnemyScripts/MossGiant.cs
@@ -7,25 +7,39 @@ public class MossGiant : Enemy
 {
     private Vector3 _currentTarget;
     private Animator _mossGiantAnim;
+    private SpriteRenderer _mossGiantSprite;
 
     private void Start()
     {
         _mossGiantAnim = GetComponentInChildren<Animator>();
+        _mossGiantSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     public override void Update()
     {
-        if (transform.position == pointA.position)
-        {
-            _currentTarget = pointB.position;
-        }
-        else if (transform.position == pointB.position)
-        {
-            _currentTarget = pointA.position;
-        }
-
         if (!_mossGiantAnim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
         {
+            if (_currentTarget == pointA.position)
+            {
+                _mossGiantSprite.flipX = true;
+            }
+            else
+            {
+                _mossGiantSprite.flipX = false;
+            }
+
+            if (transform.position == pointA.position)
+            {
+                Debug.Log("CALLED");
+                _mossGiantAnim.SetTrigger("Idle_t");
+                _currentTarget = pointB.position;
+            }
+            else if (transform.position == pointB.position)
+            {
+                _mossGiantAnim.SetTrigger("Idle_t");
+                _currentTarget = pointA.position;
+            }
+
             transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
         }
     }
