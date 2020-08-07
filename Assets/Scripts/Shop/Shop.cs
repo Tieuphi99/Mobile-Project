1c760af50f984432d7c759c7c8f41f25bd10b5bb
Tieuphi <tieuphi99@gmail.com>
2020-08-07 16:41:23 +0700

    trigger shoppanel

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject shopPanel;
+    
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(true);
+        }
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.tag == "Player")
+        {
+            shopPanel.SetActive(false);
+        }
     }
 }
