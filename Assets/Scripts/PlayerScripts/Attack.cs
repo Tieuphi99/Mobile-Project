55b5604090cfb568a100718ed2b9f8e3506b6a93
Tieuphi <tieuphi99@gmail.com>
2020-08-07 09:38:07 +0700

    skeleton damage cooldown

--- Assets/Scripts/PlayerScripts/Attack.cs
+++ Assets/Scripts/PlayerScripts/Attack.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
+    private bool _isHitable = true;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         IDamageable hit = other.GetComponent<IDamageable>();
 
-        hit?.Damage();
+        if (hit != null && _isHitable)
+        {
+            hit.Damage();
+            _isHitable = false;
+            StartCoroutine(Hit());
+        }
+    }
+
+    IEnumerator Hit()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _isHitable = true;
     }
 }
