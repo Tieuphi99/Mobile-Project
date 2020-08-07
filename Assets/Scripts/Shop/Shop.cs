6ae9d8b2bb4d2efb4765185656a9e6f3f4679463
Tieuphi <tieuphi99@gmail.com>
2020-08-07 17:43:12 +0700

    selection item

--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -11,6 +11,13 @@ public class Shop : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                UIManager.Instance.OpenShop(player.diamond);
+            }
+            
             shopPanel.SetActive(true);
         }
     }
@@ -20,6 +27,22 @@ public class Shop : MonoBehaviour
         if (other.tag == "Player")
         {
             shopPanel.SetActive(false);
+        }
+    }
+
+    public void SelectItem(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                UIManager.Instance.UpdateSelectionItem(92);
+                break;
+            case 1:
+                UIManager.Instance.UpdateSelectionItem(-16);
+                break;
+            case 2:
+                UIManager.Instance.UpdateSelectionItem(-124);
+                break;
         }
     }
 }
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("UI Manager is Null!");
+            }
+
+            return _instance;
+        }
+    }
+
+    public Text gemCountText;
+    public Image selectionImage;
+
+    public void UpdateSelectionItem(int yPos)
+    {
+        selectionImage.rectTransform.anchoredPosition =
+            new Vector2(selectionImage.rectTransform.anchoredPosition.x, yPos);
+    }
+
+    public void OpenShop(int gemCount)
+    {
+        gemCountText.text = $"{gemCount.ToString()}G";
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
