fb6ba330d2f66c5bf20ec8477bccc9f2bc284461
Tieuphi <tieuphi99@gmail.com>
2020-08-07 11:37:23 +0700

    moss giant attack complete

--- Assets/Scripts/EnemyScripts/MossGiant.cs
+++ Assets/Scripts/EnemyScripts/MossGiant.cs
@@ -9,6 +9,15 @@ public class MossGiant : Enemy, IDamageable
 
     public void Damage()
     {
+        Debug.Log($"Hit {name}");
+        Health -= 1;
+        anim.SetTrigger("Hit_t");
+        anim.SetBool("Combat_b", true);
+        
+        if (Health < 1)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public override void Init()
