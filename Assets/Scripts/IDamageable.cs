205569f2c3f96640b4abfaa88f3e67f9e7098e20
Tieuphi <tieuphi99@gmail.com>
2020-08-10 11:05:26 +0700

    minor changes

--- Assets/Scripts/IDamageable.cs
+++ Assets/Scripts/IDamageable.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamageable
+﻿public interface IDamageable
 {
     int Health { get; set; }
     void Damage();
